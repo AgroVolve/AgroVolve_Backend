2c13c9386ed01ee81422e3d924913896edc43b3d
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2025-03-06 20:33:24 +0200

    initialised repo and added dependencies

---
+++ src/main/java/com/agrovolve/agro_volve/AgroVolveApplication.java
@@ -0,0 +1,13 @@
+package com.agrovolve.agro_volve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroVolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroVolveApplication.class, args);
+	}
+
+}

0f98cef82b2a54a70783d9cd640bc25be7af55d4
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2025-03-07 14:55:26 +0200

    started working on auth

---
+++ src/main/java/com/agrovolve/agro_volve/Controller/UserController.java
@@ -0,0 +1,12 @@
+package com.agrovolve.agro_volve.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("agro-volve/api/v1/auth")
+public class UserController {
+    
+ 
+
+}
